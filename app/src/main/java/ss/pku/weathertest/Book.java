8533e4b19ee27996b0f1d6dcc664c40a21c9f04f
xieniantao <z8oo@sina.com>
2015-09-24 08:13:29 +0800

    no message

---
+++ app/src/main/java/ss/pku/weathertest/Book.java
@@ -0,0 +1,34 @@
+package ss.pku.weathertest;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+import javax.xml.transform.stream.StreamResult;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.AttributesImpl;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+/**
+ * Created by apple on 15/9/23.
+ */
+public class Book {
+    public void Parst(InputStream is) throws Exception
+    {
+//        SAXParserFactory factory = SAXParserFactory.newInstance();
+//        SAXParser parser = factory.newSAXParser();
+    }
+}
+
